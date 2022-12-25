8042064ee0d7cf84842bc3194342ade0aa7b0516
najro <orjan@najro.com>
2022-12-25 15:03:38 +0100

    first version

---
+++ schemas/allproperties.js
@@ -0,0 +1,13 @@
+export default {
+    name: 'allproperties',
+    type: 'document',
+      title: 'All Properties',
+    fields: [
+      {
+        name: 'name',
+        type: 'string',
+        title: 'Property String',
+        description: 'This is a description'
+      }
+    ]
+  }
